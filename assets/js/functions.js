2d93f9c7eda0b8fc8b764c802ebc00dfcae42e04
Romina Rivadeneira <rivrom18@gmail.com>
2018-05-19 09:11:12 -0400

    Add old found progress - refactoring

---
+++ assets/js/functions.js
@@ -0,0 +1,18 @@
+$(function() {
+  smoothScrool(300);
+})
+
+//smoothscroll function applied from document ready function
+function smoothScrool (duration) {
+  $('a[href^="#"]').on('click', function(event) {
+
+    var target = $( $(this).attr('href') );
+
+    if( target.length ) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, duration);
+    }
+  });
+}
